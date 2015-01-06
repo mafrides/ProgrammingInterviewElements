783eab6c81359813807e757818e51618963474ac
Michael Afrides <mafrides@gmail.com>
2015-01-05 21:56:54 -0800

    Refactored F# code to be less object-oriented and updated unit tests

--- ProgrammingInterviewElements.FSharp.Tests/BitOperations.Tests.fs
+++ ProgrammingInterviewElements.FSharp.Tests/BitOperations.Tests.fs
@@ -11,92 +11,70 @@ let Odds = Seq.skip 1 Odds4Bit //an odd number of odds
 let isEven x = x % 2 = 0
 let isOdd x = not <| isEven x
 
-// Tests for BitPosition Type range (uses ulong.getBit)
-
-[<Fact>]
-let bitPositionRejectsNegativePositionsWithInvalidArgs() =
-    let action() = ulong.MaxValue.getBit(BitPosition -1) |> ignore 
-    Assert.Throws<ArgumentException>(action) |> ignore
-
-[<Fact>]
-let bitPositionRejectsNegativePositionsWithInvalidArgs2() =
-    let action() = ulong.MaxValue.getBit(BitPosition Int32.MinValue) |> ignore 
-    Assert.Throws<ArgumentException>(action) |> ignore
-
-[<Fact>]
-let bitPositionRejectsPositionGreaterThan63WithInvalidArgs() =
-    let action() = ulong.MaxValue.getBit(BitPosition 64) |> ignore 
-    Assert.Throws<ArgumentException>(action) |> ignore
-
-[<Fact>]
-let bitPositionRejectsPositionGreaterThan63WithInvalidArgs2() =
-    let action() = ulong.MaxValue.getBit(BitPosition Int32.MaxValue) |> ignore 
-    Assert.Throws<ArgumentException>(action) |> ignore
-
 // Problem 5.1 Tests
 
 [<Fact>]
 let hasEvenParityPassesForEvens() =
-    Evens4Bit |> Seq.map (Assert.True << hasEvenParity)
+    Evens4Bit |> Seq.iter (Assert.True << hasEvenParity)
 
 [<Fact>]
 let hasEvenParityFailsForOdds() =
-    Odds4Bit |> Seq.map (Assert.False << hasEvenParity)  
+    Odds4Bit |> Seq.iter (Assert.False << hasEvenParity)  
 
 [<Fact>]
 let hasOddParityPassesForOdds() =
-    Odds4Bit |> Seq.map (Assert.True << hasOddParity)
+    Odds4Bit |> Seq.iter (Assert.True << hasOddParity)
 
 [<Fact>]
 let hasOddParityFailsForEvens() =
-    Evens4Bit |> Seq.map (Assert.False << hasOddParity)
+    Evens4Bit |> Seq.iter (Assert.False << hasOddParity)
 
 [<Fact>]
-let hasEvenParityIEnumerablePassesForEvens() =
-    Assert.True <| Evens4Bit.hasEvenParity()
+let hasEvenParitySeqPassesForEvens() =
+    Assert.True <| Seq.hasEvenParity Evens4Bit
 
 [<Fact>]
-let hasEvenParityIEnumerablePassesForEvenNumberOfOdds() =
+let hasEvenParitySeqPassesForEvenNumberOfOdds() =
     Assert.True <| (Seq.length Odds4Bit |> isEven)
-    Assert.True <| Odds4Bit.hasEvenParity()
+    Assert.True <| Seq.hasEvenParity Odds4Bit
 
 [<Fact>]    
-let hasEvenParityIEnumerableFailsForOddNumberOfOdds() =
+let hasEvenParitySeqFailsForOddNumberOfOdds() =
     Assert.True <| (Seq.length Odds |> isOdd)
-    Assert.False <| Odds.hasEvenParity()
+    Assert.False <| Seq.hasEvenParity Odds
 
 [<Fact>]
-let hasOddParityIEnumerablePassesForOddNumberOfOdds() =
+let hasOddParitySeqPassesForOddNumberOfOdds() =
     Assert.True <| (Seq.length Odds |> isOdd)
-    Assert.True <| Odds.hasOddParity()
+    Assert.True <| Seq.hasOddParity Odds
 
 [<Fact>]
-let hasOddParityIEnumerableFailsForEvenNumberOfOdds() =
+let hasOddParitySeqFailsForEvenNumberOfOdds() =
     Assert.True <| (Seq.length Odds4Bit |> isEven)
-    Assert.False <| Odds4Bit.hasOddParity()
+    Assert.False <| Seq.hasOddParity Odds4Bit
 
 [<Fact>]
-let hasOddParityIEnumerableFailsForEvens() =
-    Assert.False <| Evens4Bit.hasOddParity()
+let hasOddParitySeqFailsForEvens() =
+    Assert.False <| Seq.hasOddParity Evens4Bit
     
 // Problem 5.2 Tests
 
 [<Fact>]
 let swapBitsUlongReturnsIntialValueWhenBitPositionsEqual =
     let x = uint64 UInt32.MaxValue
-    Assert.Equal(x, x.swapBits (BitPosition 0) (BitPosition 0))
+    Assert.Equal(x, x |> swapBits (BitPosition 0) (BitPosition 0))
 
 [<Fact>]
 let swapBitsUlongSwapsWhenArg1GreaterThanArg2 =
     let x = uint64 UInt32.MaxValue
     let expected = (x - 1UL) ||| (1UL <<< 63)
-    Assert.Equal(expected, x.swapBits (BitPosition 63) (BitPosition 0))
+    Assert.Equal(expected, x |> swapBits (BitPosition 63) (BitPosition 0))
 
 [<Fact>]
 let swapBitsULongSwapsWhenArg1LessThanArg2 =
     let x = uint64 UInt32.MaxValue
     let expected = (x - 1UL) ||| (1UL <<< 63)
-    Assert.Equal(expected, x.swapBits (BitPosition 0) (BitPosition 63))
+    Assert.Equal(expected, x |> swapBits (BitPosition 0) (BitPosition 63))
 
 // Problem 5.3 Tests
 
@@ -109,13 +87,14 @@ type UInt64 with
             BitConverter.ToUInt64(bytes, 0)
         internalRandom
     static member randoms count =
-        Array.init count (fun _ -> ulong.random())   
+        List.init count (fun _ -> ulong.random())   
                  
 [<Fact>]
-let reverseBitsUInt64ReversesBits() = 
-    let test (x:ulong) =
-        let result = x.reverseBits()
+let reverseBitsReversesBits() = 
+    let test x =
+        let result = reverseBits x
         let resultString = Convert.ToString(int64 result,2).PadLeft(64,'0')
-        let testString = Convert.ToString(int64 x,2).PadLeft(64,'0')
-        Assert.Equal(String(Array.rev(testString.ToCharArray())), resultString)
-    ulong.randoms 30 |> Array.iter test
+        let givenString = Convert.ToString(int64 x,2).PadLeft(64,'0')
+        let givenStringReversed = String(Array.rev(givenString.ToCharArray()))
+        Assert.Equal(givenStringReversed, resultString)
+    ulong.randoms 30 |> List.iter test
--- ProgrammingInterviewElements.FSharp/BitOperations.fs
+++ ProgrammingInterviewElements.FSharp/BitOperations.fs
@@ -2,69 +2,60 @@
 
 open System
 open System.Collections.Generic
-open System.Runtime.CompilerServices
 
 type Bit = One | Zero
-
-// I would remove the exception if this were library code
-[<Struct>]
-type BitPosition(i:int) =
-    member x.Position = if (i < 0 || i > 63)
-                        then invalidArg "Position" "Bit Position Range [0-63]."
-                        else i
-
+type bitPosition = BitPosition of int
 type ulong = UInt64
 
-type UInt64 with
-    member x.getBit (i:BitPosition) =
-        if (x &&& (1UL <<< i.Position) = 0UL) then Zero else One            
-    member x.setBitTo value (i:BitPosition) =
-        match value with
-        | One ->  x ||| (1UL <<< i.Position)
-        | Zero -> x &&& ~~~(1UL <<< i.Position)
-    member x.setBit (i:BitPosition) = x.setBitTo One
-    member x.clearBit (i:BitPosition) = x.setBitTo Zero
-    //Solution to Problem 5.2 as extension to UInt64
-    member x.swapBits i j = 
-        let y = x.setBitTo (x.getBit i) j
-        y.setBitTo (x.getBit j) i
-    //Solution to Problem 5.3 as extension to UInt64
-    member x.reverseBits =
-        let Masks = 
-            [| 0x00000000ffffffffUL, 32;
-               0x0000ffff0000ffffUL, 16;
-               0x00ff00ff00ff00ffUL,  8;
-               0x0f0f0f0f0f0f0f0fUL,  4;
-               0x3333333333333333UL,  2;
-               0x5555555555555555UL,  1|]
-        let reverseBitsInternal() =
-            let num = ref x
-            let flip(mask,offset) = 
-                num := ((!num >>> offset) &&& mask) ||| ((!num <<< offset) &&& (mask <<< offset))
-            Masks |> Array.iter flip               
-            !num
-        reverseBitsInternal
+let getBit (BitPosition i) x =
+    if (x &&& (1UL <<< i) = 0UL) then Zero else One
 
-let hasEvenParity (x:ulong) =
+let setBitTo value (BitPosition i) x =
+    match value with
+    | One -> x ||| (1UL <<< i)
+    | Zero -> x &&& ~~~(1UL <<< i)
+
+let setBit = setBitTo One
+let clearBit = setBitTo Zero
+let getHighBit = getBit (BitPosition 63)
+let getLowBit = getBit (BitPosition 0)
+
+//Solution to Prob 5.1
+let hasEvenParity x =
     let powersOf2_1To32 = Seq.unfold (fun i -> if i >= 64 then None else Some(i, i * 2)) 1
     let bitwiseXOrInHighBit = powersOf2_1To32 |> Seq.fold (fun acc offset -> acc ^^^ (acc <<< offset)) x 
-    bitwiseXOrInHighBit.getBit(BitPosition 63) = Zero
+    getHighBit bitwiseXOrInHighBit = Zero
 
-let hasOddParity (x:ulong) =
-    not <| hasEvenParity x
+let hasOddParity = not << hasEvenParity
 
-//Solutions to Prob 5.1 Expressed as extensions to IEnumberbale<ulong>
-[<Extension>]
-type IEnumerable() =
-    [<Extension>]
-    static member inline hasEvenParity(collection:IEnumerable<ulong>) =
-        hasEvenParity <| Seq.reduce (fun x y -> x ^^^ y) collection
-    //Could not figure out how to call one extension method from another
-    //not sure why not <| collection.hasEvenParity() does not work
-    //or why not << IEnumerable<ulong>.hasEvenParity shouldn't work
-    [<Extension>]
-    static member inline hasOddParity(collection:IEnumerable<ulong>) =
-        hasOddParity <| Seq.reduce (fun x y -> x ^^^ y) collection 
+module Seq =
+    let hasEvenParity xs = 
+        hasEvenParity <| Seq.reduce (fun x y -> x ^^^ y) xs
+    
+    let hasOddParity xs =
+        not <| hasEvenParity xs
+
+//Solution to Problem 5.2
+let swapBits i j x =
+    let newI = getBit j x
+    let newJ = getBit i x
+    let y = setBitTo newI i x
+    setBitTo newJ j y
+
+//Solution to Problem 5.3
+let reverseBits = 
+    let Masks = 
+        [ 0x00000000ffffffffUL, 32
+          0x0000ffff0000ffffUL, 16
+          0x00ff00ff00ff00ffUL,  8
+          0x0f0f0f0f0f0f0f0fUL,  4
+          0x3333333333333333UL,  2
+          0x5555555555555555UL,  1]
+    let reverseBitsInternal x =
+        let flip num (mask,offset) = 
+            ((num >>> offset) &&& mask) ||| ((num <<< offset) &&& (mask <<< offset))
+        Masks |> List.fold flip x               
+    reverseBitsInternal 
     
     
 
